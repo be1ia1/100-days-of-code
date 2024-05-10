7c4e4892019712ff48e19efc89c3b5e142f3359b
gulaa <gula@pr.km.ua>
2024-05-10 12:25:37 +0300

    day29 half

---
+++ password-manager-start/main.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    pass
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+    site = entry_website.get()
+    email = entry_email.get()
+    password = entry_password.get()
+    with open('database.txt', 'a') as fo:
+        fo.write(f'{site} | {email} | {password}\n')
+    entry_website.delete(0, tk.END)
+    entry_password.delete(0, tk.END)
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = tk.Tk()
+window.title('Password Manager')
+window.config(padx=50, pady=50)
+
+canvas = tk.Canvas(width=200, height=200)
+photo = tk.PhotoImage(file='logo.png')
+canvas.create_image(100, 100, image=photo)
+canvas.grid(column=1,row=0)
+
+label_website = tk.Label(text='Website:')
+label_website.grid(column=0, row=1)
+
+entry_website = tk.Entry(width=35)
+entry_website.grid(column=1, row=1, columnspan=2, sticky=tk.EW)
+entry_website.focus()
+
+label_email = tk.Label(text='Email/Username:')
+label_email.grid(column=0, row=2)
+
+entry_email = tk.Entry(width=35)
+entry_email.grid(column=1, row=2, columnspan=2, sticky=tk.EW)
+entry_email.insert(0, 'belial.ahula@gmail.com')
+
+label_password = tk.Label(text='Password:')
+label_password.grid(column=0, row=3)
+
+entry_password = tk.Entry(width=21)
+entry_password.grid(column=1, row=3, sticky=tk.EW)
+
+button_password = tk.Button(text='Generate Password', command=generate_password)
+button_password.grid(column=2, row=3)
+
+button_add = tk.Button(text='Add', width=36, command=save_password)
+button_add.grid(column=1, row=4, columnspan=2, sticky=tk.EW)
+
+
+window.mainloop()
