4d19f2e7fed337a461db4dde85ff1b5ac2bd2f34
Andrii Hula <belial.ahula@gmail.com>
2025-07-05 20:57:11 +0300

    some tasks

--- blackjack_start.py
+++ blackjack_start.py
@@ -1,77 +1,146 @@
-import sys
 from random import choice
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 
-def show_cards(name, info):
-    print(f"{name.title()} have:")
-    for card in info['cards']:
-        print(card) 
-    print(f"Sum is {sum(info['cards'])}")
+def ace_transform(cards):
+    one_index = cards.index(11)
+    cards.remove(11)
+    cards.insert(one_index, 1)
 
-def give_one_more_card():
-    player['cards'].append(choice(cards))
-    show_cards('player', player)
 
-cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+while True:
+    another_game = input(
+        "Do you want to play a game of Blackjack? (y/n): ").strip().lower()
+    if another_game == 'y':
+        print("Starting a new game of Blackjack...")
+        player_cards = [choice(cards), choice(cards)]
+        player_sum = sum(player_cards)
+        dealer_cards = [choice(cards),]
 
-one_more_game = True
+        if player_sum > 21 and 11 in player_cards:
+            ace_transform(player_cards)
+        print(
+            f"Your cards: {player_cards}, current score: {sum(player_cards)}")
+        print(f"Dealer's first card: {dealer_cards[0]}")
 
-while one_more_game:
-    dealer = {
-        'cards': [choice(cards), choice(cards)]
-    }
-
-    player = {
-        'cards': [choice(cards), choice(cards)]
-    }
-
-    game_not_over = True
-    while game_not_over:
-        show_cards('player', player)
-        one_more_card = True
-        while one_more_card:
-            sum_player = sum(player['cards'])
-            ask_card = input('Do you want one more card? ')
-            if ask_card == 'y':
-                give_one_more_card()
-                if 11 in player['cards']:
-                    value_ace = int(input("Enter ace value: "))
-                    if value_ace == 1:
-                        index_ace = player['cards'].index(11)
-                        player['cards'][index_ace] = 1
-                        sum_player += 1
-                        show_cards('player', player)
-                sum_player = sum(player['cards'])
-                if sum_player > 21:
-                    print(f"You lose with {player['cards']}")
-                    one_more_card = False
-                    game_not_over = False
+        while sum(player_cards) < 21:
+            another_card = input(
+                "Type 'y' to get another card, type 'n' to pass: ").strip().lower()
+            if another_card == 'y':
+                player_cards.append(choice(cards))
+                print(
+                    f"Your cards: {player_cards}, current score: {sum(player_cards)}")
             else:
-                one_more_card = False
-        if game_not_over == False:
-            break
-        print('-----------------------------------')
-        print('No more cards')
-        print('-----------------------------------')
-        show_cards('dealer', dealer)
-        sum_dealer = sum(dealer['cards'])
-        if sum_dealer < 17:
-            print('Dealer sum is less then 17.. One more card to dealer..')
-            dealer['cards'].append(choice(cards))
-            sum_dealer = sum(dealer['cards'])
-            show_cards('dealer', dealer)
-        
-        if sum_dealer > 21:
-            print(f"Dealer lose with {dealer['cards']}")
-        elif sum_dealer == sum_player:
-            print('Draw..')
-        elif sum_dealer > sum_player:
-            print('Dealer wins')
-        elif sum_dealer < sum_player:
-            print('Player wins')
-        game_not_over = False
+                break
+            if sum(player_cards) > 21:
+                if 11 in player_cards:
+                    ace_transform(player_cards)
+                    print(
+                        f"Your cards: {player_cards}, current score: {sum(player_cards)}")
+                else:
+                    print("You went over 21! You lose!")
+                    break
+        if sum(player_cards) <= 21:
+            dealer_cards.append(choice(cards))
+            print(
+                f"Dealer's cards: {dealer_cards}, current score: {sum(dealer_cards)}")
+            while sum(dealer_cards) < 17:
+                dealer_cards.append(choice(cards))
+                print(
+                    f"Dealer's cards: {dealer_cards}, current score: {sum(dealer_cards)}")
+                if sum(dealer_cards) > 21:
+                    if 11 in dealer_cards:
+                        ace_transform(dealer_cards)
+                        print(
+                            f"Dealer's cards: {dealer_cards}, current score: {sum(dealer_cards)}")
+                        continue
+                    else:
+                        print("Dealer went over 21! You win!")
+            if sum(dealer_cards) == sum(player_cards):
+                print("It's a draw!")
+            elif sum(dealer_cards) > sum(player_cards):
+                print("Dealer wins!")
+            elif sum(dealer_cards) < sum(player_cards):
+                print("You win!")
+    else:
+        print("Thank you for playing! Goodbye!")
+        break
 
-    one_more = input("One more game? ")
-    if one_more == 'n':
-        print('Goodbye!')
-        sys.exit()
+
+# import sys
+# from random import choice
+
+
+# def show_cards(name, info):
+#     print(f"{name.title()} have:")
+#     for card in info['cards']:
+#         print(card)
+#     print(f"Sum is {sum(info['cards'])}")
+
+# def give_one_more_card():
+#     player['cards'].append(choice(cards))
+#     show_cards('player', player)
+
+# cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+
+# one_more_game = True
+
+# while one_more_game:
+#     dealer = {
+#         'cards': [choice(cards), choice(cards)]
+#     }
+
+#     player = {
+#         'cards': [choice(cards), choice(cards)]
+#     }
+
+#     game_not_over = True
+#     while game_not_over:
+#         show_cards('player', player)
+#         one_more_card = True
+#         while one_more_card:
+#             sum_player = sum(player['cards'])
+#             ask_card = input('Do you want one more card? ')
+#             if ask_card == 'y':
+#                 give_one_more_card()
+#                 if 11 in player['cards']:
+#                     value_ace = int(input("Enter ace value: "))
+#                     if value_ace == 1:
+#                         index_ace = player['cards'].index(11)
+#                         player['cards'][index_ace] = 1
+#                         sum_player += 1
+#                         show_cards('player', player)
+#                 sum_player = sum(player['cards'])
+#                 if sum_player > 21:
+#                     print(f"You lose with {player['cards']}")
+#                     one_more_card = False
+#                     game_not_over = False
+#             else:
+#                 one_more_card = False
+#         if game_not_over == False:
+#             break
+#         print('-----------------------------------')
+#         print('No more cards')
+#         print('-----------------------------------')
+#         show_cards('dealer', dealer)
+#         sum_dealer = sum(dealer['cards'])
+#         if sum_dealer < 17:
+#             print('Dealer sum is less then 17.. One more card to dealer..')
+#             dealer['cards'].append(choice(cards))
+#             sum_dealer = sum(dealer['cards'])
+#             show_cards('dealer', dealer)
+
+#         if sum_dealer > 21:
+#             print(f"Dealer lose with {dealer['cards']}")
+#         elif sum_dealer == sum_player:
+#             print('Draw..')
+#         elif sum_dealer > sum_player:
+#             print('Dealer wins')
+#         elif sum_dealer < sum_player:
+#             print('Player wins')
+#         game_not_over = False
+
+#     one_more = input("One more game? ")
+#     if one_more == 'n':
+#         print('Goodbye!')
+#         sys.exit()
--- guess_the_number.py
+++ guess_the_number.py
@@ -1,40 +1,68 @@
-import sys
 from random import randint
+import pyfiglet
 
+banner = pyfiglet.figlet_format('Guess The Number')
+print(banner)
 print("Welcome to the Number Guessing Game!")
-
-def game_round():
-    number = randint(1,100)
-    # print(number)
-    print("I'm thinking of a number between 1 and 100.")
-    # difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
-    difficulty = 'easy'
-    attempts = 10 if difficulty == 'easy' else 5
-
-    player_wins = False
-    while attempts > 0:
-        print(f"You have {attempts} attempts.")
-        player_guess = int(input("Make a guess: "))
-        if player_guess == number:
-            player_wins = True
-            break
-        if player_guess > number:
-            print("Too high.")
-            attempts -= 1
-        if player_guess < number:
-            print("Too low.")
-            attempts -= 1
+print("I'm thinking of a number between 1 to 100.")
+number = randint(1, 100)
+print(f"Pssst, the correct answer is: {number}")
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard':")
+attempts = 10 if difficulty == 'easy' else 5
+game_continue = True
+while game_continue and attempts > 0:
+    print(f"You have {attempts} attempts remaining to guess the number.")
+    answer = int(input("Make a guess: "))
+    if answer == number:
+        print(f"You got it! The answer was {answer}.")
+        game_continue = False
+    elif answer > number:
+        print("Too high.")
+        attempts -= 1
+    elif answer < number:
+        print("Too low.")
+        attempts -= 1
+if attempts == 0 and answer != number:
+    print("You lose.")
 
 
-    if player_wins:
-        print("You win!")
-    else:
-        print(f"You lose.. The number is: {number}")
-    one_more = input("One more game: ")
-    if one_more == 'y':
-        game_round()
-    else:
-        print("Goodbye!")
-        sys.exit()
+# import sys
+# from random import randint
 
-game_round()
+# print("Welcome to the Number Guessing Game!")
+
+# def game_round():
+#     number = randint(1,100)
+#     # print(number)
+#     print("I'm thinking of a number between 1 and 100.")
+#     # difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+#     difficulty = 'easy'
+#     attempts = 10 if difficulty == 'easy' else 5
+
+#     player_wins = False
+#     while attempts > 0:
+#         print(f"You have {attempts} attempts.")
+#         player_guess = int(input("Make a guess: "))
+#         if player_guess == number:
+#             player_wins = True
+#             break
+#         if player_guess > number:
+#             print("Too high.")
+#             attempts -= 1
+#         if player_guess < number:
+#             print("Too low.")
+#             attempts -= 1
+
+
+#     if player_wins:
+#         print("You win!")
+#     else:
+#         print(f"You lose.. The number is: {number}")
+#     one_more = input("One more game: ")
+#     if one_more == 'y':
+#         game_round()
+#     else:
+#         print("Goodbye!")
+#         sys.exit()
+
+# game_round()
