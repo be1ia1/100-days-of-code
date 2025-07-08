3c104d696073d5aa11231cb37e902e6c73592211
Andriy Gula <gula@pr.km.ua>
2025-07-08 22:47:36 +0300

    day 15 - redo

--- coffee_mashine.py
+++ coffee_mashine.py
@@ -1,73 +1,161 @@
 from coffee_mashine_start import MENU, resources
+import sys
 
+def user_decision():
+    decision = input("What would you like? (espresso/latte/cappuccino): ")
+    return decision
 
-def user_input():
-    '''Ask user about drink to make'''
-    choose = input("What would you like? (espresso/latte/cappuccino): ")
-    return choose
+def get_report():
+    for k, v in resources.items():
+        print(f"{k.title()}: {v}")
 
-def check_resources(drink):
-    '''Check if machine have enough resources to make drink'''
-    check_list = []
-    for k, v in MENU[drink]['ingredients'].items():
-        if v <= resources.get(k):
-            check_list.append(True)
-        else:
-            check_list.append(False)
-            print(f"Sorry there is not enough {k}")
-    return all(check_list)
-
-def calculate_coins():
-    '''Calculate user coints'''
-    print('Please insert coins.')
-    quaters = int(input("How many quaters?: "))
+def process_coins():
+    quarters = int(input("How many quarters?: "))
     dimes = int(input("How many dimes?: "))
     nickles = int(input("How many nickles?: "))
     pennies = int(input("How many pennies?: "))
-    # quaters = 12
-    # dimes = 12
-    # nickles = 12
-    # pennies = 12
-    user_sum = quaters * 0.25 + dimes * 0.1 + nickles * 0.05 + pennies * 0.01
-    return round(user_sum, 2)
+    all_sum = quarters * 0.25 + dimes * 0.1 + nickles * 0.05 + pennies * 0.01
+    return all_sum
 
-def check_transaction(user_sum, user_input):
-    drink_cost = MENU[user_input]["cost"]
-    if user_sum >= drink_cost:
+def check_transaction(drink, money):
+    if MENU[drink]["cost"] <= money:
         return True
-    return False
 
-def transaction(user_sum, user_input):
-    drink_cost = MENU[user_input]["cost"]
-    change = round(user_sum - drink_cost, 2)
-    resources['money'] += drink_cost
-    if change:
-        print(f"Here is ${change} in change.")
+def check_resources(drink='latte'):
+    for k, v in MENU[drink]['ingredients'].items():
+        if v > resources[k]:
+            return k
 
-def make_coffee(user_input):
-    for k, v in MENU[user_input]['ingredients'].items():
+def make_coffee(drink):
+    for k, v in MENU[drink]['ingredients'].items():
         resources[k] -= v
-    return f"Here is your {user_input}. Enjoy!"
+    print(f"Here is your {drink}!")
 
-coffee_machine_on = True
-while coffee_machine_on:
-    next_step = user_input()
-    if next_step == 'off':
-        coffee_machine_on = False
-    elif next_step == 'report':
-        for k, v in resources.items():
-            if k == 'water' or k == 'milk':
-                print(f"{k.title()}: {v}ml")
-            elif k == 'coffee':
-                print(f"{k.title()}: {v}g")
-            elif k == 'money':
-                print(f"{k.title()}: ${v}")
+is_online = True
+while is_online:
+    answer = user_decision()
+    if answer == 'off':
+        sys.exit()
+    elif answer == 'report':
+        get_report()
     else:
-        drink = next_step
-        if check_resources(drink):
-            user_sum = calculate_coins()
-            if check_transaction(user_sum, drink):
-                transaction(user_sum, drink)
-                print(make_coffee(drink))
+        chk = check_resources(answer)
+        if chk:
+            print(f"Sorry there is not enough {chk}")
+        else:
+            user_money = round(process_coins(), 2)
+            if check_transaction(answer, user_money):
+                resources["money"] += MENU[answer]["cost"]
+                change = round(user_money - MENU[answer]["cost"], 2)
+                print(f'Your change: {change}')
+                make_coffee(answer)
             else:
-                print('Sorry, not enough coins..')
+                print(f'You insert {user_money}, but {answer} costs {MENU[answer]["cost"]}')
+                print(f'Return your {user_money}, please, insert more money..')         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def user_input():
+#     '''Ask user about drink to make'''
+#     choose = input("What would you like? (espresso/latte/cappuccino): ")
+#     return choose
+
+# def check_resources(drink):
+#     '''Check if machine have enough resources to make drink'''
+#     check_list = []
+#     for k, v in MENU[drink]['ingredients'].items():
+#         if v <= resources.get(k):
+#             check_list.append(True)
+#         else:
+#             check_list.append(False)
+#             print(f"Sorry there is not enough {k}")
+#     return all(check_list)
+
+# def calculate_coins():
+#     '''Calculate user coints'''
+#     print('Please insert coins.')
+#     quaters = int(input("How many quaters?: "))
+#     dimes = int(input("How many dimes?: "))
+#     nickles = int(input("How many nickles?: "))
+#     pennies = int(input("How many pennies?: "))
+#     # quaters = 12
+#     # dimes = 12
+#     # nickles = 12
+#     # pennies = 12
+#     user_sum = quaters * 0.25 + dimes * 0.1 + nickles * 0.05 + pennies * 0.01
+#     return round(user_sum, 2)
+
+# def check_transaction(user_sum, user_input):
+#     drink_cost = MENU[user_input]["cost"]
+#     if user_sum >= drink_cost:
+#         return True
+#     return False
+
+# def transaction(user_sum, user_input):
+#     drink_cost = MENU[user_input]["cost"]
+#     change = round(user_sum - drink_cost, 2)
+#     resources['money'] += drink_cost
+#     if change:
+#         print(f"Here is ${change} in change.")
+
+# def make_coffee(user_input):
+#     for k, v in MENU[user_input]['ingredients'].items():
+#         resources[k] -= v
+#     return f"Here is your {user_input}. Enjoy!"
+
+# coffee_machine_on = True
+# while coffee_machine_on:
+#     next_step = user_input()
+#     if next_step == 'off':
+#         coffee_machine_on = False
+#     elif next_step == 'report':
+#         for k, v in resources.items():
+#             if k == 'water' or k == 'milk':
+#                 print(f"{k.title()}: {v}ml")
+#             elif k == 'coffee':
+#                 print(f"{k.title()}: {v}g")
+#             elif k == 'money':
+#                 print(f"{k.title()}: ${v}")
+#     else:
+#         drink = next_step
+#         if check_resources(drink):
+#             user_sum = calculate_coins()
+#             if check_transaction(user_sum, drink):
+#                 transaction(user_sum, drink)
+#                 print(make_coffee(drink))
+#             else:
+#                 print('Sorry, not enough coins..')
--- higher_lower.py
+++ higher_lower.py
@@ -1,43 +1,87 @@
-import sys
-from random import randint
-
 from art import logo, vs
 from game_data import data
+from random import choice
 
+print(logo)
 
-def get_person(number):
-    return data[number]
+first = choice(data)
 
 score = 0
-game_over = False
+game_not_over = True
 
 
-def game_round(person1, person2):
-    print(f"Compare A: {person1["name"]}, a {
-          person1["description"]}, from {person1["country"]}.")
-    print(f"Against B: {person2["name"]}, a {
-          person2["description"]}, from {person2["country"]}.")
+def compare_variants(variant_a, variant_b, user_answer):
+    if variant_a['follower_count'] > variant_b['follower_count'] and user_answer == 'a':
+        return variant_a
+    if variant_a['follower_count'] < variant_b['follower_count'] and user_answer == 'b':
+        return variant_b
+    if variant_a['follower_count'] == variant_b['follower_count']:
+        return variant_a if user_answer == 'a' else variant_b
+    return False
 
-    user_answer = input("Who has more followers? Type 'A' or 'B': ").lower()
 
-    if user_answer == 'a':
-        if person1["follower_count"] > person2["follower_count"]:
-            return person1
-    elif user_answer == 'b':
-        if person2["follower_count"] > person1["follower_count"]:
-            return person2
+def game_round(variant_a):
+    variant_b = choice(data)
+    while variant_b == variant_a:
+        variant_b = choice(data)
+    print(f"Compare A: {variant_a['name']}, a {variant_a['description']}, from {variant_a['country']}.")
+    print(vs)
+    print(f"Compare B: {variant_b['name']}, a {variant_b['description']}, from {variant_b['country']}.")
+    user_answer = (input('Who has more followers? Type "A" or "B": ')).lower()
+    result = compare_variants(first, variant_b, user_answer)
+    return result
 
-while not game_over:
-    if score == 0:
-        person1 = get_person(randint(0, len(data)-1))
-    person2 = get_person(randint(0, len(data)-1))
-    result = game_round(person1, person2)
-    if not result:
-        print(f"{person1["name"]} has {person1["follower_count"]} vs {
-              person2["name"]} has {person2["follower_count"]}")
-        print(f"Final score is: {score}")
-        print("Game Over..")
-        game_over = True
+
+while game_not_over:
+    round_result = game_round(first)
+    if not round_result:
+        print(f'Game over.. Score: {score}')
+        game_not_over = False
     else:
-        person1 = result
         score += 1
+        first = round_result
+
+
+# import sys
+# from random import randint
+
+# from art import logo, vs
+# from game_data import data
+
+
+# def get_person(number):
+#     return data[number]
+
+# score = 0
+# game_over = False
+
+
+# def game_round(person1, person2):
+#     print(f"Compare A: {person1["name"]}, a {
+#           person1["description"]}, from {person1["country"]}.")
+#     print(f"Against B: {person2["name"]}, a {
+#           person2["description"]}, from {person2["country"]}.")
+
+#     user_answer = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+#     if user_answer == 'a':
+#         if person1["follower_count"] > person2["follower_count"]:
+#             return person1
+#     elif user_answer == 'b':
+#         if person2["follower_count"] > person1["follower_count"]:
+#             return person2
+
+# while not game_over:
+#     if score == 0:
+#         person1 = get_person(randint(0, len(data)-1))
+#     person2 = get_person(randint(0, len(data)-1))
+#     result = game_round(person1, person2)
+#     if not result:
+#         print(f"{person1["name"]} has {person1["follower_count"]} vs {
+#               person2["name"]} has {person2["follower_count"]}")
+#         print(f"Final score is: {score}")
+#         print("Game Over..")
+#         game_over = True
+#     else:
+#         person1 = result
+#         score += 1
